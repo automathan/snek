ff45a59aaaba5beef3523a1af39bde87a9719f27
Nosferamen <jona.jorg@gmail.com>
2021-04-18 20:46:56 +0200

    Tailchaser AI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import random
 from agent import Agent
+import agent_tailchaser
 from snek.environment import Snek
 
 if '--novid' in sys.argv:
@@ -12,6 +13,8 @@ def main():
     agent = Agent(env)
 
     if '--test' not in sys.argv:
+        total_len = 0
+        num_episodes = 0
         state = env.reset()
 
         while True:
@@ -20,7 +23,10 @@ def main():
             env.render() # Comment out this call to train faster
 
             if done:
+                total_len += env.player.len
+                num_episodes += 1
                 state = env.reset()
+                print('Average len: {:.1f}'.format(total_len / num_episodes))
 
 if __name__ == '__main__':
     main()
